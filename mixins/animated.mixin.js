f5b15b28d85c0f1e94bc50fab782081d6e8e9d4c
Stanley Chen <stanley@interlo.co>
2018-07-11 14:25:04 -0500

    Adds optional debugging code for animations.

--- mixins/animated.mixin.js
+++ mixins/animated.mixin.js
@@ -1,11 +1,26 @@
 import { TimelineMax, TweenMax } from 'gsap';
 import { mapGetters } from 'vuex';
 
+const DEBUG = '';
+
 export default {
+  data() {
+    return {
+      animatedLast: ''
+    }
+  },
   methods: {
-    animate(name, options) {
+    animate(name, options = {}) {
       let element = this.$refs[this.animationTarget];
-      if (element) {
+      if (element && (options.when ? options.when === this.animatedLast :
+          (!options.unless || options.unless !== this.animatedLast))) {
+        if (this.animationTarget === DEBUG) {
+          console.info('Animation', name, '{--');
+          options.onComplete = options.onComplete || (() => {
+            console.info('Animation', name, ' --}');
+          });
+        }
+        this.animatedLast = name;
         return _.reduce(this.animationDefinitions[name], (timeline, [time, style]) => {
           if (time) {
             return timeline.to(element, time, style);
@@ -13,6 +28,10 @@ export default {
             return timeline.from(element, time, style);
           }
         }, new TimelineMax(options)).duration(this.animationDuration).play(0);
+      } else if (this.animationTarget === DEBUG) {
+        if (options.unless) {
+          console.info('Prevented', name, 'because', options.unless);
+        }
       }
     },
     set(properties) {
