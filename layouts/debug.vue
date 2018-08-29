c48507622dacf19c2a8bae541e9995a3899b1327
Stanley Chen <stanley@interlo.co>
2018-08-28 18:47:47 -0700

    Refines debug menu and removes extraneous styling.

--- layouts/debug.vue
+++ layouts/debug.vue
@@ -1,9 +1,8 @@
 <template lang="pug">
-  .content
+  .page-frame
     nuxt.page
     .menu
       nuxt-link(v-for="(link, label) in views", :to="link", :key="link") {{ label }}
-
 </template>
 
 <script>
@@ -11,14 +10,12 @@
     data() {
       return {
         views: {
-          Home: "/",
-          About: '/about',
           Sound: '/sound',
+          Backing: '/backing',
           Grid: '/grid',
           Stage: '/stage',
-          Lesson: '/lesson',
           Progress: '/progress',
-          Backing: '/backing'
+          Lesson: '/lesson'
         }
       };
     }
@@ -33,25 +30,17 @@
   .menu
     posit(absolute, 0, 0, x, 0);
     background-color: main-blue;
-    display: block;
-    height: 48px;
     padding: 0 1vw 0 content-side-margin;
     overflow-x: scroll;
     white-space: nowrap;
 
     a
-      display: inline-block;
       color: white;
       font-size: 16px;
       line-height: 48px;
       margin-right: 50px;
       text-decoration: none;
-      vertical-align: middle;
-      cursor: pointer;
 
       &.nuxt-link-active
         font-weight: 800;
-
-    .links
-      padding-top: 15px;
 </style>
