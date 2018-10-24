454e8d9692e0a95c7d4cfbaa275d5e2550d1e632
Stanley Chen <stanley@interlo.co>
2018-10-23 22:50:31 -0700

    Improved snare and kick sounds.

--- common/sound/percussion.sound.js
+++ common/sound/percussion.sound.js
@@ -1,21 +1,20 @@
 import Tone from '../tone';
 
-const membraneEnvelopeOptions = {
-  attack: 0.001,
-  decay: 0.1,
-  sustain: 0
-};
-
 export class KickSound {
   constructor() {
     this.kick = new Tone.MembraneSynth({
-      pitchDecay: 0.02,
-      oscillator: {type: 'square4'},
-      envelope: membraneEnvelopeOptions,
+      pitchDecay: 0.05,
+      octaves: 8,
+      oscillator: {type: 'sine'},
+      envelope: {
+        attack: 0.001,
+        decay: 0.1,
+        sustain: 0.1,
+        release: 0.1
+      },
       volume: 10
     });
-    let chorus = new Tone.Chorus(4, 3, 1);
-    this.kick.chain(chorus, Tone.Master);
+    this.kick.chain(Tone.Master);
   }
 
   play(time) {
@@ -25,29 +24,34 @@ export class KickSound {
 
 export class SnareSound {
   constructor() {
-    this.hit = new Tone.MembraneSynth({
-      pitchDecay: 0.01,
-      envelope: membraneEnvelopeOptions,
-      volume: 10
-    });
-    let hitEffect = new Tone.Distortion(0.1).toMaster();
-    this.hit.chain(hitEffect, Tone.Master);
-    this.rattle = new Tone.NoiseSynth({
-      type: 'brown',
-      envelope: {
-        attack: 0.01,
-        decay: 0.001,
-        sustain: 0.001,
-        release: 0.05
+    this.hit = new Tone.PolySynth(6, Tone.Synth, {
+      volume: 5,
+      oscillator: {
+        partials: [0, 2, 3, 4]
       },
-      volume: -10
+      envelope: {
+        attack: 0.003,
+        decay: 0.07,
+        sustain: 0,
+      },
     });
-    let snareEffect = new Tone.BitCrusher(1).toMaster();
-    this.rattle.chain(snareEffect, Tone.Master);
+    this.hit.chain(Tone.Master);
+
+    this.rattle = new Tone.NoiseSynth({
+      type: 'white',
+      envelope: {
+        attack: 0.005,
+        decay: 0.15,
+        sustain: 0
+      },
+      volume: 0
+    });
+    const lowPass = new Tone.Filter({ frequency: 9000 });
+    this.rattle.chain(lowPass, Tone.Master);
   }
 
   play(time) {
-    this.hit.triggerAttackRelease('A4', '16n', time);
+    this.hit.triggerAttackRelease(['D4', 'F#4', 'E4'], '16n', time);
     this.rattle.triggerAttackRelease('16n', time);
   }
 }
@@ -58,7 +62,11 @@ export class ClickSound {
       pitchDecay: 0.01,
       octaves: 6,
       oscillator: {type: 'square4'},
-      envelope: membraneEnvelopeOptions,
+      envelope: {
+        attack: 0.001,
+        decay: 0.1,
+        sustain: 0
+      },
       volume: -20
     });
     this.click.connect(Tone.Master);
