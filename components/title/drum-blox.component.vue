f49ba56230c229dab85bc01d7fe82685a6008da3
Stanley Chen <stanley@interlo.co>
2018-11-12 18:48:35 -0800

    Updates home page with simple title component.

---
+++ components/title/drum-blox.component.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+  h1.title DRUM#[br]BLOX
+  <!--svg.title(height="250", width="450", viewBox="-225 -125 450 250")-->
+    <!--path(v-for="(path, letter) in letters", :d="path", :class="letter")-->
+    <!--circle.note(v-for="note in notes", :cx="note.cx", :cy="note.cy", :r="note.r")-->
+</template>
+
+<script>
+
+  const quarter = 30;
+  const eighth = 12;
+
+  export default {
+    constants: {
+      letters: {
+        d: 'M-200,-100 h50 a50,50 0 0,1 0,100 h-50 Z',
+        r: 'M-100,-100 h70 a30,30 0 0,1 0,60 a30,40 0 0,1 30,40 h-100 Z',
+        u: 'M0,-100 m0,15 h100 v35 a50,50 0 0,1 -100,0 Z',
+        m: 'M100,-100 m0,40 a25,25 0 0,1 50,0 a25,25 0 0,1 50,0 v50 h-100 Z',
+        b: 'M-200,0 h75 a25,25 0 0,1 0,50 a25,25 0 0,1 0,50 h-75 Z',
+        l: 'M-100,0 h40 v40 a60,30 0 0,0 60,30 v25 h-100 Z',
+        o: 'M0,0 m0,50 a50,50 0 0,1 100,0 a50,50 0 0,1 -100,0',
+        x: 'M100,0 m20,0 l30,30 l30,-30 l20,20 l-30,30 l30,30 l-20,20 l-30,-30 l-30,30 l-20,-20 l30,-30 l-30,-30 Z'
+      },
+      notes: {
+        '00:000': { note: 'kick', cx: -150, cy: -50, r: quarter },
+        '01:096': { note: 'snare', cx: -30, cy: -60, r: eighth },
+        '02:000': { note: 'kick', cx: 50, cy: -50, r: quarter },
+        '03:000': { note: 'snare', cx: 130, cy: -60, r: eighth },
+        '03:096': { note: 'snare', cx: 170, cy: -60, r: eighth },
+        '04:000': { note: 'snare', cx: -170, cy: 25, r: eighth },
+        '04:096': { note: 'kick', cx: -130, cy: 75, r: eighth },
+        '06:000': { note: 'kick', cx: 50, cy: 50, r: quarter }
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" type="text/stylus">
+  .title
+    background-color: primary-blue;
+    border: solid 2vh back-blue;
+    color: white;
+    font-size: 8vw;
+    line-height: 7vw;
+    padding: 2vh 4vh;
+    text-shadow: 3px 3px 3px dark-grey;
+
+  // future game color palette?
+  drumblox-background-color = #00486b;
+  drumblox-color1 = #308887;
+  drumblox-color2 = #76bf9d;
+  drumblox-color3 = #a6daaa;
+  drumblox-color4 = #cff1a4;
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,59 @@
 <template lang="pug">
   .content
-    h3
-      nuxt-link.button(to="/a1") A1
-      | Under development.
+    .games
+      nuxt-link.button(to="/a1")
+        drum-blox
+    svg.triangle(height="30", width="30", viewBox="0 0 40 40")
+      path(d="M20,0 L40,40, L0,40 Z")
+    .text
+      p This game is a #[strong work in progress]
+      p Make sure your #[strong sound is turned up!]
+      p Works best with Chrome, mobile may be wonky
+    .feedback Comments? Questions? Feedback?#[br]feedback@flatthirteen.com
 </template>
 
 <script>
+  import DrumBlox from '~/components/title/drum-blox.component';
+
   export default {
+    components: {
+      'drum-blox': DrumBlox
+    },
     layout: 'public'
   }
 </script>
 
 <style scoped lang="Stylus" type="text/stylus">
   .content
-    margin: 3vw content-side-margin;
     font-family: primary-font;
     position: relative;
+    text-align: center;
 
-    .button
-      background-color: main-blue;
-      border-radius: 50%;
-      color: main-blue;
-      font-size: 1em;
+  .games
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    margin: 0 3vh;
+
+    a
+      margin: 2vh;
       text-decoration: none;
-      margin: -1vw -5vw;
-      padding: 1vw;
-      position: absolute;
-      height: 2vw;
-      width: 2vw;
-      text-align: center;
 
-      &:hover
-        color: white;
+  .triangle
+    fill: primary-green;
+    margin: auto;
+
+  .text
+    margin: 10px;
+
+    p
+      font-size: 3vw;
+
+  .feedback
+    background-color: main-blue;
+    color: white;
+    font-size: 15px;
+    text-align: center;
+    margin-top: 20px;
+    padding: 20px;
 </style>
