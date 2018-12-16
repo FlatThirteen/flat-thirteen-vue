88e4f1560700299823e66ec4a87da653ffd43450
Stanley Chen <stanley@interlo.co>
2018-12-15 22:43:14 -0800

    Fixes upper case library reference.

--- common/tone.js
+++ common/tone.js
@@ -2,7 +2,7 @@ let Tone = { rightNow() {} };
 
 if (process.browser) {
   // Load Tone only in browser
-  Tone = require('Tone');
+  Tone = require('tone');
   Tone.rightNow = function() {
     return Tone.context.currentTime;
   }
